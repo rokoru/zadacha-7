0666358ad482abd19d182e2881bb4836a8eed7a9
Dmitry <gomerty15@mail.ru>
2021-04-19 13:10:35 +0300

    Добавленно решение

---
+++ zadacha-7/zadacha-7/zadacha-7.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <ctime>
+
+
+
+int main() {
+
+	srand(time(NULL));
+
+	const int size = 7;
+	int U[size];
+	int D[size];
+	int V[size];
+	int S[size];
+
+
+	for (int j = 0; j < size; j++) {
+
+		U[j] = rand() % 10;
+		D[j] = rand() % 15;
+		V[j] = rand() % 5;
+	}
+	for (int i = 0; i < 7; i++) {
+		S[i] = (U[i] + D[i] + V[i]) / 3;
+	}
+	int s;
+	for (int e = 0; e < size; e++) {
+		s += S[e];
+	}
+	int s1;
+	s1 = s / 7;
+
+	std::cout << s1 << std::endl;
+
+	return 0;
+}
